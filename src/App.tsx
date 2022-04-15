b4c9b8f8daddeeb7ea3f1b7c4d46e3042b886c7b
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-04-15 22:38:50 +0100

    Create test page and test build.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,17 @@ import React from 'react'
 
 export default function App() {
 	return (
-		<h1>React App Template</h1>
+		<main>
+			<h1>React App Template</h1>
+			<p>A template for building static React apps in Typescript.</p>
+			<p>I could use <code>create-react-app</code>, but I don't like the bloat...</p>
+			<p>Includes styled-components.</p>
+		</main>
 	)
 }
+
+// A template for building static React apps in Typescript.
+
+// I could use `create-react-app`, but I don't like the bloat...
+
+// Includes styled-components.
