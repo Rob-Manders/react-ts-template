9d69caf316bcf5049557eacc51eb6b3f1bbdd4b0
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-04-15 22:09:37 +0100

    Set up Webpack build tools and config.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # React Project Template
 
-A template for building React apps in Typescript.
+A template for building static React apps in Typescript.
 
 I could use `create-react-app`, but I don't like the bloat...
+
+Includes styled-components.
---
+++ bundler/webpack.common.js
@@ -0,0 +1,36 @@
+
+const path = require('path')
+
+module.exports = {
+	entry: './src/index.tsx',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.(ts|tsx)$/,
+				exclude: /node_modules/,
+				loader: ['ts-loader']
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx', '.ts', '.tsx']
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true
+	}
+}
