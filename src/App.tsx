925264107bf53fdd92a4ae3be6b086b4da88de86
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-06-19 14:18:07 +0100

    Prettier config.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-
 import React from 'react'
 import test from './assets/test.jpg'
 
 export default function App() {
-	return (
-		<main>
-			<h1>React App Template</h1>
-			<p>A template for building static React apps in Typescript.</p>
-			<p>I could use <code>create-react-app</code>, but I don't like the bloat...</p>
-			<p>Includes styled-components.</p>
+  return (
+    <main>
+      <h1>React App Template</h1>
+      <p>A template for building static React apps in Typescript.</p>
+      <p>
+        I could use <code>create-react-app</code>, but I don't like the bloat...
+      </p>
+      <p>Includes styled-components.</p>
 
-			<img src={test} alt="test image" />
-		</main>
-	)
+      <img src={test} alt='test image' />
+    </main>
+  )
 }
-
